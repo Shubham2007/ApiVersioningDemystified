a0a0cde277e5d92e936c71253148d488709ba3a7
Shubham Chauhan <shubham.chauhn@gmail.com>
2021-03-30 21:51:09 +0530

    Added Swagger with versioning support

--- ApiVersioningDemo/Startup.cs
+++ ApiVersioningDemo/Startup.cs
@@ -1,12 +1,16 @@
 using ApiVersioningDemo.Controllers;
+using ApiVersioningDemo.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ApiVersioningDemo
 {
@@ -31,10 +35,10 @@ namespace ApiVersioningDemo
                 //options.ApiVersionReader = new QueryStringApiVersionReader("v");
                 //options.ApiVersionReader = new HeaderApiVersionReader("version");
                 options.ApiVersionReader = ApiVersionReader.Combine(
-                    new QueryStringApiVersionReader("v"),
-                    new QueryStringApiVersionReader("version"),
-                    new HeaderApiVersionReader("v"),
-                    new UrlSegmentApiVersionReader()
+                    new QueryStringApiVersionReader("v")
+                    //new QueryStringApiVersionReader("version"),
+                    //new HeaderApiVersionReader("v"),
+                    //new UrlSegmentApiVersionReader()
                     );
 
                 //options.ApiVersionReader = new UrlSegmentApiVersionReader();
@@ -43,11 +47,20 @@ namespace ApiVersioningDemo
                     //.Action(c => c.GetV3_0()).MapToApiVersion(1, 1);
             });
 
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options => options.OperationFilter<SwaggerDefaultValues>());
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -55,6 +68,17 @@ namespace ApiVersioningDemo
             }
 
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
 
             app.UseAuthorization();
 
